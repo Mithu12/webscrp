e27b982b578021c361ca1d97c81573e3b73547eb
Mithu12 <mosaddekalimithu123@gmail.com>
2022-08-01 18:07:43 +0600

    retrieving all the image "src"

--- main.py
+++ main.py
@@ -3,17 +3,23 @@ import requests
 url = 'https://www.vecteezy.com/free-photos'
 req = requests.get(url)
 
-a_string = req.text
-indices = [index for index in range(len(a_string)) if a_string.startswith('<img', index)]
-total = 0
-for i in indices:
-    c = i
-    tag = ''
-    while a_string[c] != '>':
-        tag += a_string[c]
-        c += 1
-    total += 1
-    tag += a_string[c]
-    print(tag)
+response_str = req.text
 
-print('Total {} tags'.format(total))
+# retrieving starting index of all img tags
+start_indexes = [index for index in range(len(response_str)) if response_str.startswith('<img', index)]
+
+for i in start_indexes:
+    # skipping '<img characters
+    c = i + 4
+    src = ''
+    while response_str[c] != '>':
+        if response_str.startswith('src=', c):
+            # skipping 'src="' characters
+            srcStart = c + 5
+            while response_str[srcStart] != '"':
+                src = src + response_str[srcStart]
+                srcStart += 1
+            print(src)
+        c += 1
+
+print('Total {} image src'.format(len(start_indexes)))
